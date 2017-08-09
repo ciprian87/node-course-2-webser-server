21195e5805926f8005fa853ef4291fb0230b25b2
ciprian87 <ciprianrobu@Ciprians-Mac-mini.local>
2017-08-08 20:47:44 -0500

    Added a projects page

--- server.js
+++ server.js
@@ -40,6 +40,13 @@ app.get('/about', (req, res) => {
     });
 })
 
+app.get('/projects', (req, res) => {
+    res.render('projects.hbs', {
+        pageTitle: 'Projects Page',
+        bodyMessage: "Your projects will be here"
+    })
+});
+
 app.get('/', (req, res) => {
     res.render('home.hbs', {
         pageTitle: 'Home Page',
